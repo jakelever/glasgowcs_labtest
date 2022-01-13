8cbcb628dfe318cc86309bbd96332e9dd5151398
Jake Lever <jake.lever@gmail.com>
2022-01-13 14:36:41 +0000

    Added a testcase

--- glasgowcs_labtest/tad2022/lab1/testsuite.py
+++ glasgowcs_labtest/tad2022/lab1/testsuite.py
@@ -11,3 +11,15 @@ class multiply_by_three_test(LabTest):
 		]
 		
 		self.run_testcases(testcases)
+
+class tokenize_strsplit_test(LabTest):
+	def runTest(self):
+		testcases = [
+			{'input':'John's father didn’t have £100.', 'output': ["John's", 'father', 'didn’t', 'have', '£100.']},
+			{'input':'Where we're going, we won't need roads.', 'output': ['Where', "we're", 'going,', 'we', "won't", 'need', 'roads.']},
+			{'input':'I'm going to make him an offer he can't refuse.', 'output': ["I'm", 'going', 'to', 'make', 'him', 'an', 'offer', 'he', "can't", 'refuse.']},
+			{'input':'It's-a Me, Mario!', 'output': ["It's-a", 'Me,', 'Mario!']},
+			{'input':'Mess with the best, die like the rest.', 'output': ['Mess', 'with', 'the', 'best,', 'die', 'like', 'the', 'rest.']},
+		]
+		
+		self.run_testcases(testcases)
