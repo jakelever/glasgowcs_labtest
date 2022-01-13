50bcf2e46e4e182b383bd52fa729e2b39bc167a8
Jake Lever <jake.lever@gmail.com>
2022-01-13 14:06:46 +0000

    Switch to setuptools

--- setup.py
+++ setup.py
@@ -2,11 +2,14 @@
 
 from distutils.core import setup
 
-setup(name='uofgcs_labtest',
-      version='1.0.0',
-      description='Testing tools for University of Glasgow CS courses',
-      author='Jake Lever',
-      author_email='jake.lever@glasgow.ac.uk',
-      url='https://jakelever.github.io/',
-      packages=['uofgcs_labtest'],
-     )
+from setuptools import setup
+
+setup(name='glasgowcs_labtest',
+	version='1.0.1',
+	description='Testing framework for Glasgow CS classes',
+	url='https://github.com/jakelever/glasgowcs_labtest',
+	author='Jake Lever',
+	author_email='jake.lever@glasgow.ac.uk',
+	license='MIT',
+	packages=['glasgowcs_labtest'])
+	
