574489086348dc7ce5dfe73cc3707c009df3c950
Jake Lever <jake.lever@gmail.com>
2022-01-13 16:00:10 +0000

    Fix multiply by three testcase

--- glasgowcs_labtest/tad2022/lab1/testsuite.py
+++ glasgowcs_labtest/tad2022/lab1/testsuite.py
@@ -3,11 +3,11 @@ from glasgowcs_labtest.utils import LabTest
 class multiply_by_three_test(LabTest):
 	def runTest(self):
 		testcases = [
-			{'input': 1, 'output': 3},
-			{'input': 2, 'output': 6},
-			{'input': 3, 'output': 9},
-			{'input': 4, 'output': 12},
-			{'input': 5, 'output': 15},
+			{'input': (1,), 'output': 3},
+			{'input': (2,), 'output': 6},
+			{'input': (3,), 'output': 9},
+			{'input': (4,), 'output': 12},
+			{'input': (5,), 'output': 15},
 		]
 		
 		self.run_testcases(testcases)
