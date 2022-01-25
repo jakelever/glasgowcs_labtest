1878cf1dc52c657e485d8d4cf7df8e0d0b4cfec0
Jake Lever <jake.lever@gmail.com>
2022-01-25 12:19:14 +0000

    Bump version number (for floating point comparison fix)

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from distutils.core import setup
 from setuptools import setup,find_packages
 
 setup(name='glasgowcs_labtest',
-	version='1.0.2',
+	version='1.0.3',
 	description='Testing framework for Glasgow CS classes',
 	url='https://github.com/jakelever/glasgowcs_labtest',
 	author='Jake Lever',
