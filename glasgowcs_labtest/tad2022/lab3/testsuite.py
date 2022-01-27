dca9bdde3d576a8925a0eefd036b7a173ed4b092
Jake Lever <jake.lever@gmail.com>
2022-01-27 10:51:31 +0000

    Add empty lab3 code

---
+++ glasgowcs_labtest/tad2022/lab3/testsuite.py
@@ -0,0 +1,13 @@
+
+def make_tests():
+	return {
+
+"multiply_by_three": [
+	{'input': (1,), 'output': 3},
+	{'input': (2,), 'output': 6},
+	{'input': (3,), 'output': 9},
+	{'input': (4,), 'output': 12},
+	{'input': (5,), 'output': 15},
+],
+
+}
