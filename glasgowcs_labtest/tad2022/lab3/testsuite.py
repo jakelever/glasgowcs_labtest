e6db56ff41ee6fb5712da409ed18c20033e9899f
Jake Lever <jake.lever@gmail.com>
2022-01-31 18:27:06 +0000

    Add a test for unigram prob

--- glasgowcs_labtest/tad2022/lab3/testsuite.py
+++ glasgowcs_labtest/tad2022/lab3/testsuite.py
@@ -2,12 +2,12 @@
 def make_tests():
 	return {
 
-"multiply_by_three": [
-	{'input': (1,), 'output': 3},
-	{'input': (2,), 'output': 6},
-	{'input': (3,), 'output': 9},
-	{'input': (4,), 'output': 12},
-	{'input': (5,), 'output': 15},
+"unigram_prob": [
+	{'input': ('cup', {'cup': 2, 'spoon': 2, 'glass': 6, 'fork': 1}, 11), 'output': 0.18181818181818182},
+	{'input': ('plate', {'plate': 2, 'cup': 2, 'fork': 5, 'spoon': 5}, 14), 'output': 0.14285714285714285},
+	{'input': ('fork', {'bowl': 4, 'fork': 5, 'plate': 3, 'cup': 1}, 13), 'output': 0.38461538461538464},
+	{'input': ('cup', {'plate': 2, 'glass': 3, 'fork': 1, 'cup': 1}, 7), 'output': 0.14285714285714285},
+	{'input': ('glass', {'glass': 5, 'spoon': 3}, 8), 'output': 0.625}
 ],
 
 }
