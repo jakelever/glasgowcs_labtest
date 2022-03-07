9a758702608c761e0d5437a7bab84fae864b4565
Jake Lever <jake.lever@gmail.com>
2022-03-07 14:18:07 +0000

    Bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from distutils.core import setup
 from setuptools import setup,find_packages
 
 setup(name='glasgowcs_labtest',
-	version='1.0.5',
+	version='1.0.6',
 	description='Testing framework for Glasgow CS classes',
 	url='https://github.com/jakelever/glasgowcs_labtest',
 	author='Jake Lever',
